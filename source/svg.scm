137509c89db09a4f99207df1b8759bd5f12753b6
slemal <sim.lemal007@gmail.com>
2021-04-10 19:40:01 +0200

    Corrected typo and enhanced svg.scm

--- source/svg.scm
+++ source/svg.scm
@@ -10,20 +10,31 @@
 ; of the given drawing in a (width x height) viewport
 (define drawing->svg
   (lambda (width height drawing)
-    (string-join (list (format "<svg width=\"~a\" height=\"~a\" viewbox=\"~a ~a ~a ~a\" />" width height
-                               (drawing.minx drawing) (drawing.miny drawing)
-                               (- (drawing.maxx drawing) (drawing.minx drawing))
-                               (- (drawing.maxy drawing) (drawing.miny drawing)))
-                       (string-join (map polyline->svg (drawing.polylines drawing))
-                                    "\n\t" #:before-first "\t")
-                       "</svg>") "\n")))
+    (let* ((minx (drawing.minx drawing))
+          (miny (drawing.miny drawing))
+          (rangex (- (drawing.maxx drawing) minx))
+          (rangey (- (drawing.maxy drawing) miny)))
+      (string-join (list (format "<svg width=\"~a\" height=\"~a\" viewBox=\"~a ~a ~a ~a\">"
+                                 width height (~r minx #:precision 5) (~r miny #:precision 5)
+                                 (~r rangex #:precision 5) (~r rangey #:precision 5))
+                         (format "<rect x=\"~a\" y=\"~a\" width=\"100%\" height=\"100%\" fill=\"white\"/>"
+                                 (~r minx #:precision 5) (~r miny #:precision 5))
+                         (string-join (map (lambda (polyline)
+                                             (polyline->svg polyline
+                                                            (/ (sqrt (max rangex rangey)) 100)))
+                                           (drawing.polylines drawing))
+                                      "\n")
+                         "</svg>")
+                   "\n"))))
 
-; if polyline is a polyline then (polyline->svg) returns the SVG representation
-; as a string of the given polyline
+; if polyline is a polyline and width is a positive number then
+; (polyline->svg) returns the SVG representation as a string of
+; the given polyline with the given width
 (define polyline->svg
-  (lambda (polyline)
-    (string-append "<polyline points=\""
-                   (string-join
-                    (map (lambda (p) (string-append (~a (car p)) "," (~a (cdr p)))) polyline)
-                    " ")
-                   "\"/>")))
+  (lambda (polyline width)
+    (string-join
+     (map (lambda (p) (string-append (~r (car p) #:precision 5) ", " (~r (cdr p) #:precision 5)))
+          polyline)
+     "\n"
+     #:before-first "<polyline points=\"\n"
+     #:after-last (format "\n\" fill=\"none\" stroke=\"black\" stroke-width=\"~a\"/>" width))))
