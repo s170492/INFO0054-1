5f02cbb00d42a7aba8b8b32cea88c42816d485ac
slemal <sim.lemal007@gmail.com>
2021-04-10 13:54:50 +0200

    Implemented svg.scm

--- source/svg.scm
+++ source/svg.scm
@@ -10,4 +10,20 @@
 ; of the given drawing in a (width x height) viewport
 (define drawing->svg
   (lambda (width height drawing)
-    "TODO"))
+    (string-join (list (format "<svg width=\"~a\" height=\"~a\" viewbox=\"~a ~a ~a ~a\" />" width height
+                               (drawing.minx drawing) (drawing.miny drawing)
+                               (- (drawing.maxx drawing) (drawing.minx drawing))
+                               (- (drawing.maxy drawing) (drawing.miny drawing)))
+                       (string-join (map polyline->svg (drawing.polylines drawing))
+                                    "\n\t" #:before-first "\t")
+                       "</svg>") "\n")))
+
+; if polyline is a polyline then (polyline->svg) returns the SVG representation
+; as a string of the given polyline
+(define polyline->svg
+  (lambda (polyline)
+    (string-append "<polyline points=\""
+                   (string-join
+                    (map (lambda (p) (string-append (~a (car p)) "," (~a (cdr p)))) polyline)
+                    " ")
+                   "\"/>")))
